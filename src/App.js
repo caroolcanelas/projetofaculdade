6e431800d7bab3119e55de8df708a96166f731c2
caroolcanelas <carolina.canelas@soulasalle.com>
2024-12-15 20:24:24 -0300

    Criar navbar e footer

--- src/App.js
+++ src/App.js
@@ -3,27 +3,29 @@ import Home from './pages/Home'
 import Empresa from './pages/Empresa'
 import Contato from './pages/Contato'
 import NewProject from './pages/NewProject'
+import Projetos from './pages/Projetos'
+
+
 import Container from './components/layout/Container'
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
+
 
 
 function App() {
   return (
     <Router>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/empresa">Empresa</Link>
-        <Link to="/contato">Contato</Link>
-        <Link to="/newProject">newProject</Link>
-      </div>
+      <Navbar/>
       <Container customClass="minHeight">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/projetos" element={<Projetos />} />
           <Route path="/empresa" element={<Empresa />} />
           <Route path="/contato" element={<Contato />} />
           <Route path="/newProject" element={<NewProject />} /> 
         </Routes>
       </Container>
-      <p>Footer</p>
+      <Footer />
     </Router>
   )
 }
