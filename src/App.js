f2b00fcf25605ba4df14140fafd17575c534eda4
caroolcanelas <carolina.canelas@soulasalle.com>
2024-12-29 17:48:06 -0300

    Criação do banco e dos módulos de serviço e projeto

--- src/App.js
+++ src/App.js
@@ -1,33 +1,32 @@
-import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
-import Home from './components/pages/Home'
-import Empresa from './components/pages/Empresa'
-import Contato from './components/pages/Contato'
-import NewProject from './components/pages/NewProject'
-import Projetos from './components/pages/Projetos'
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./components/pages/Home";
+import Empresa from "./components/pages/Empresa";
+import Contato from "./components/pages/Contato";
+import NewProject from "./components/pages/NewProject";
+import Projetos from "./components/pages/Projetos";
+import Projeto from "./components/pages/Projeto";
 
-
-import Container from './components/layout/Container'
-import Navbar from './components/layout/Navbar'
-import Footer from './components/layout/Footer'
-
-
+import Container from "./components/layout/Container";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
 
 function App() {
   return (
     <Router>
-      <Navbar/>
+      <Navbar />
       <Container customClass="minHeight">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/projetos" element={<Projetos />} />
           <Route path="/empresa" element={<Empresa />} />
           <Route path="/contato" element={<Contato />} />
-          <Route path="/newProject" element={<NewProject />} /> 
+          <Route path="/newProject" element={<NewProject />} />
+          <Route path="/projetos/:id" element={<Projeto />} />
         </Routes>
       </Container>
       <Footer />
     </Router>
-  )
+  );
 }
 
 export default App;
--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,25 +1,77 @@
+import { useState, useEffect } from 'react'
+
 import styles from './ProjectForm.module.css'
 import Input from '../form/Input'
 import Select from '../form/Select'
 import SubmitButton from '../form/SubmitButton'
 
 
-function ProjectForm({btnText}){
-    return(
-        <form className={styles.form}>
-            <Input 
+function ProjectForm({ handleSubmit, btnText, projectData }) {
+
+    const [categories, setCategories] = useState([])
+    const [project, setProject] = useState(projectData || {} )
+
+    useEffect(() => { //pra solicitar dados o useEffect é melhor (executa só uma vez)
+        fetch("http://localhost:8089/categories",
+            {
+                method: "GET",
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            }
+        )
+            .then((resp) => resp.json()) //transformo resposta em json
+            .then((data)=>{
+                setCategories(data)
+            }) //pego os dados em json e jogo no hook de setCategories 
+            .catch((err) => console.log(err))
+    }, [])
+
+    const submit = (e) => {
+        e.preventDefault()
+        handleSubmit(project)    
+    }
+
+    function handleChange(e){
+        setProject({...project, [e.target.name]: e.target.value })
+        
+    } //altera o valor de um objeto/entidade que estamos inserindo no banco enquanto digitamos
+
+    function handleCategory(e){
+        setProject({...project, category: {
+            id: e.target.value,
+            name: e.target.options[e.target.selectedIndex].text,
+        }
+         })
+        
+    } //altera o valor de um objeto/entidade que estamos inserindo no banco enquanto digitamos
+
+    return (
+        <form onSubmit={submit} className={styles.form}>
+            <Input
                 type="text"
                 text="Nome do curso"
                 name="name"
                 placeholder="Insira o nome do curso"
+                handleOnChange={handleChange}
+                value={project.name ? project.name: ''}
             />
-            <Input 
+            <Input
                 type="number"
                 text="Número de horas do curso"
-                name="name"
+                name="horas"
                 placeholder="Insira o número de horas do curso"
+                handleOnChange={handleChange}
+                value={project.horas ? project.horas: ''}
+
             />
-            <Select name="category_id" text="Selecione a área do curso"/>
+            <Select 
+            name="category_id" 
+            text="Selecione a área do curso" 
+            options={categories} 
+            handleOnChange={handleCategory}
+            value={project.category ? project.category.id: ''}
+            />
             <SubmitButton text={btnText} />
 
 
